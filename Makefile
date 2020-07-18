d6a27224f3d2175d7fe8efbb31492b9132e1601f
David Wilson <davidwil@posteo.de>
2020-07-18 21:50:24 +0100

    endlimit. first go. a bit rough

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CC=tcc
 
-all: bin/mycat bin/echo bin/false bin/limit bin/true 
+all: bin/mycat bin/echo bin/endlimit bin/false bin/limit bin/true 
 
 bin/mycat: cat.c
 	$(CC) -o bin/mycat cat.c
 
 bin/echo: echo.c
 	$(CC) -o bin/echo echo.c
+
+bin/endlimit: endlimit.c
+	$(CC) -o bin/endlimit endlimit.c
 
 bin/false: false.c
 	$(CC) -o bin/false false.c
---
+++ endlimit.c
@@ -0,0 +1,83 @@
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* prog;
+
+int get_number_of_lines(char* strIn);
+int get_number_of_lines_in_file(char* strIn);
+void limit_stdin(int number_of_lines);
+
+int main(int argc, char* argv[]) {
+  char *str;
+  long nlines;
+
+  if (argc!=2) {
+    fprintf(stderr, "%s: Usage: %s NUMBER_OF_LINES\n", argv[0], argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  prog = argv[0];
+  str = argv[1];
+  nlines = get_number_of_lines_in_file(str);
+  limit_stdin(nlines);
+
+}
+
+int get_number_of_lines_in_file(char* strIn) {
+  char *endptr;
+  long nlines;
+
+  nlines = strtol(strIn, &endptr, 10);
+
+  errno = 0;
+
+  if ((errno == ERANGE && (nlines == LONG_MAX || nlines == LONG_MIN))
+      || (errno !=0 && nlines == 0)) {
+    perror("strtol");
+    exit(EXIT_FAILURE);
+  }
+
+  if (endptr==strIn || *endptr!= '\0') {
+    fprintf(stderr, "%s: %s: not a valid number of lines\n", prog, strIn);
+    exit(EXIT_FAILURE);
+  }
+
+  return nlines;
+}
+
+void limit_stdin(int number_of_lines) {
+  int i,x;
+  int number_to_print;
+  ssize_t read;
+  char* line = NULL;
+  size_t len = 0;
+  char** file_content;
+  
+  file_content = malloc(1 * sizeof(char*));
+
+  i=0;
+  for (1;;) {
+    if ((read = getline(&line, &len, stdin)==-1)) {
+      break;
+    }
+    
+    file_content = (char**) realloc(file_content, ((i+1) * sizeof(char*)));
+    file_content[i] = malloc(strlen(line) *sizeof(char*));
+
+    strcpy(file_content[i], line);
+    i++;
+  }
+
+  number_to_print = number_of_lines<i ? number_of_lines : i;
+  for (x=0;x<number_to_print;x++) {
+    printf("Line %d: %s", x, file_content[i-1-x]);
+  }
+  for(x=0; x<i; x++) {
+    free(file_content[x]);
+  }
+
+  free(file_content);
+}
